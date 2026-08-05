1173ffa6e520c6e25e3d6a4ab5f987101c322646
Haytham <104620852+HaythamSaba@users.noreply.github.com>
2026-08-05 23:46:04 +0200

    fixing shadow color in dark mode

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -253,7 +253,7 @@ export function DashboardPage() {
 
         {/* Habits + Chart */}
         <div className="flex flex-col xl:flex-row justify-between items-stretch gap-3 sm:gap-4 lg:gap-6">
-          <div className="card bg-[#fefefe] dark:bg-gray-950 rounded-2xl p-3 sm:p-4 lg:p-6 hover:shadow-lg hover:shadow-primary-200 dark:shadow-gray-900 border border-gray-200 dark:border-gray-700 xl:flex-1">
+          <div className="card bg-[#fefefe] dark:bg-gray-950 rounded-2xl p-3 sm:p-4 lg:p-6 hover:shadow-lg hover:shadow-primary-100 dark:hover:shadow-primary-900 hover:dark:shadow-lg dark:shadow-gray-900 border border-gray-200 dark:border-gray-700 xl:flex-1">
             <HabitsContainer filteredHabits={filteredHabits} />
           </div>
           <div className="w-full xl:w-1/2">
